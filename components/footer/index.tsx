bface624cbef53c38d2d47ee7bb0f747fe18e6e1
Pham Van Duong <83070538+frey-eth@users.noreply.github.com>
2024-09-10 21:53:03 +0700

    feat: change X

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -4,7 +4,7 @@ import { ImTelegram } from "react-icons/im";
 const Footer = () => {
   return (
     <div className="w-full p-5 flex items-center justify-center flex-row gap-10 ">
-      <a href="https://x.com/eBakeryfun" target="_blank" className="text-white">
+      <a href="https://x.com/eBakeryfunX" target="_blank" className="text-white">
         <FaSquareXTwitter size={48} />
       </a>
       <a href="https://t.me/eBakeryFun" target="_blank" className="text-white">
